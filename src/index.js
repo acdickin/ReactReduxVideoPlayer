4ee38dd8d70b5dc3207298f85699c3ecce4649ad
Andrew Dickinson <andrew.dickinson@t2.local>
2017-05-03 20:03:58 -0700

    Selected video is shown and search works

--- src/index.js
+++ src/index.js
@@ -11,20 +11,30 @@ class App extends Component{
 	constructor(props){
 		super(props);
 
-		this.state = { videos: [] }
-		
-		YTSearch({ key:API_KEY, term:'red panda'}, (videos)=>{
-			this.setState({videos});
-		// this.setState ({videos:videos})
-		});
+		this.state = { 
+			videos: [],
+		  selectedVideo:null
+		}
+		this.videoSearch('kittens');
 	}
 
+	videoSearch(term){
+		YTSearch({ key:API_KEY, term:term}, (videos)=>{
+			this.setState({
+				videos:videos,
+				selectedVideo:videos[0]
+			});
+		});
+	}
 	render(){
 		return(
 			<div>
-		  	<SearchBar/>
-		  	<VideoDetail video={this.state.videos[0]} />
-		  	<VideoList videos={this.state.videos} />
+		  	<SearchBar onSearchTermChange={term=>this.videoSearch(term)} />
+		  	<VideoDetail video={this.state.selectedVideo} />
+		  	<VideoList 
+					onVideoSelect = {selectedVideo=>this.setState({selectedVideo}) }
+		  		videos= {this.state.videos}
+		  	 />
 		 	</div>
 	  );
 	} 
